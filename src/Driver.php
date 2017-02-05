56a6116e3779c93d817c9f77b5df6e86784eb702
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2017-02-05 13:03:06 +0200

    Finished testing

--- src/Driver.php
+++ src/Driver.php
@@ -1,15 +1,29 @@
 <?php
 namespace OAuth2;
 
+/**
+ * Encapsulates operations to perform on an OAuth2 vendor.
+ */
 class Driver {
 	private $clientInformation;
 	private $serverInformation;
 
+	/**
+	 * Creates a driver instance. 
+	 * 
+	 * @param ClientInformation $clientInformation
+	 * @param ServerInformation $serverInformation
+	 */
 	public function __construct(ClientInformation $clientInformation, ServerInformation $serverInformation) {
 		$this->clientInformation = $clientInformation;
 		$this->serverInformation = $serverInformation;
 	}
 
+	/**
+	 * Calls OAuth2 vendor to create an authorization code and redirect it to ClientInformation.getSiteURL()
+	 * 
+	 * @param array $scopes List of scopes to request grant to.
+	 */
 	public function getAuthorizationCode($scopes) {
 		$acr = new AuthorizationCodeRequest($this->serverInformation->getAuthorizationEndpoint());
 		$acr->setClientInformation($this->clientInformation);
@@ -18,6 +32,13 @@ class Driver {
 		$acr->execute(new RedirectionExecutor());
 	}
 
+	/**
+	 * Calls OAuth2 to create an access token based on authorization code received
+	 * 
+	 * @param string $authorizationCode Authorization code received from OAuth2 vendor.
+	 * @param ResponseWrapper $resposeWrapper Encapsulates logic of reading OAuth2 vendor response.
+	 * @return \OAuth2\AccessTokenResponse
+	 */
 	public function getAccessToken($authorizationCode, ResponseWrapper $resposeWrapper) {
 		$atr = new AccessTokenRequest($this->serverInformation->getTokenEndpoint());
 		$atr->setClientInformation($this->clientInformation);
@@ -27,6 +48,15 @@ class Driver {
 		return new AccessTokenResponse($resposeWrapper->getResponse());
 	}
 
+	/**
+	 * Gets resource from OAuth2 application based on access token.
+	 * 
+	 * @param string $accessToken Access token received fromn OAuth2 vendor.
+	 * @param string $resourceURL URL to resource needed to get information from.
+	 * @param array $fields Fields to retrieve from that resource.
+	 * @param ResponseWrapper $resposeWrapper Encapsulates logic of reading OAuth2 vendor response.
+	 * @return mixed Returns resource as string or array.
+	 */
 	public function getResource($accessToken, $resourceURL, $fields=array(), ResponseWrapper $resposeWrapper) {
 		$we = new WrappedExecutor($resposeWrapper);
 		$we->setHttpMethod(HttpMethod::GET);
