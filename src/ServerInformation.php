366328689c2fad180ed87ebff8fbe230d9a295c0
Lucian Popescu <lucian.gabriel.popescu@gmail.com>
2017-02-05 13:03:06 +0200

    Finished testing

--- src/ServerInformation.php
+++ src/ServerInformation.php
@@ -1,19 +1,37 @@
 <?php
 namespace OAuth2;
 
+/**
+ * Encapsulates information about OAuth2 server.
+ */
 class ServerInformation {
 	private $authorizationEndpoint;
 	private $tokenEndpoint;
 	
+	/**
+	 * Sets information necessary to request authorization codes and access tokens
+	 * 
+	 * @param string $authorizationEndpoint
+	 * @param string $tokenEndpoint
+	 */
 	public function __construct($authorizationEndpoint, $tokenEndpoint) {
 		$this->authorizationEndpoint = $authorizationEndpoint;
 		$this->tokenEndpoint = $tokenEndpoint;
 	}
 	
+	/**
+	 * Gets URL to authorization code request endpoint.
+	 * 
+	 * @return string
+	 */
 	public function getAuthorizationEndpoint() {
 		return $this->authorizationEndpoint;
 	}
 	
+	/**
+	 * Gets URL to access token request endpoint.
+	 * @return string
+	 */
 	public function getTokenEndpoint() {
 		return $this->tokenEndpoint;
 	}
